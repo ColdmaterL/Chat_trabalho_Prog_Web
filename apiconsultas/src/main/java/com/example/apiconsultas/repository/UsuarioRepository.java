1997d465e9a98d01d94e806bedc35338d520929d
Lucas Mendonça <67527025+ColdmaterL@users.noreply.github.com>
2023-07-20 23:55:51 -0300

    Delete apiconsultas directory

--- apiconsultas/src/main/java/com/example/apiconsultas/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.apiconsultas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.apiconsultas.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	Usuario findByEmail(String email);
-}
